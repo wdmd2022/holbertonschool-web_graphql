c9f17aa5eb3f6c30a7609ac7fadf7385d50fb89b
Douglas <wdmdavison@icloud.com>
2023-11-13 22:51:39 +0000

    connected to mongo

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema/schema.js')
+const connection_string = 'mongodb+srv://cooluser:rootytooty@cluster0.awaadm7.mongodb.net/?retryWrites=true&w=majority';
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -13,3 +15,10 @@ app.use('/graphql',graphqlHTTP({
 app.listen(4000,()=>{
   console.log('now listening for request on port 4000');
 });
+
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+  }
+);
+
+mongoose.connect(connection_string);
