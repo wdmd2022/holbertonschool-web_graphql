41ebe211a1dc63e6ffe9f6c91d976d87429fbf4f
Douglas <wdmdavison@icloud.com>
2023-11-13 19:21:58 +0000

    working queries on 2

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -7,7 +7,8 @@ const schema = require('./schema/schema.js')
 const app = express();
 
 app.use('/graphql',graphqlHTTP({
-  schema: schema
+  schema: schema,
+  graphiql: true
 }));
 app.listen(4000,()=>{
   console.log('now listening for request on port 4000');
--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -1,11 +1,24 @@
 // here we will talk about some cool object stuff
 
 const {GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema} = require('graphql');
+const lodash = require('lodash');
+
+let tasks = [
+    {id: '1',
+    title: 'Create your first webpage',
+    weight: 1,
+    description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)'},
+    {id: '2',
+    title: 'Structure your webpage',
+    weight: 1,
+    description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order'}
+];
 
 const TaskType = new GraphQLObjectType({
     name: 'Task',
     fields: {
         id: { type: GraphQLString },
+        title: { type: GraphQLString },
         weight: { type: GraphQLInt },
         description: { type: GraphQLString }
     }
@@ -18,27 +31,16 @@ const RootQuery = new GraphQLObjectType({
             type: TaskType,
             args: {
                 id: { type: GraphQLString }
+            },
+            resolve: (parent, args) => {
+                return lodash.find(tasks, {id: args.id});
             }
         }
     },
-    resolve(parent, args) {
-        return 10;
-    }
 });
 
 const schema = new GraphQLSchema({
     query: RootQuery
 });
-
-let tasks = [
-    {id: '1',
-    title: 'Create your first webpage',
-    weight: 1,
-    description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)'},
-    {id: '2',
-    title: 'Structure your webpage',
-    weight: 1,
-    description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order'}
-];
 
 module.exports = schema;
